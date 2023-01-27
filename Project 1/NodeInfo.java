da966f1895f809930d7a87b02df7a69841e51203
tam499 <tam499@nau.edu>
2023-01-27 11:21:09 -0700

    Create NodeInfo.java
    Created NodeInfo.java

---
+++ Project 1/NodeInfo.java
@@ -0,0 +1,37 @@
+package chat;
+
+import java.io.Serializable;
+
+public class NodeInfo implements Serializable
+{
+    String ipAddress;
+    int portNumber;
+    String name = null;
+
+    public NodeInfo(String ipAddress, int portNumber, String name)
+    {
+        this.ipAddress = ipAddress;
+        this.portNumber = portNumber;
+        this.name = name;
+    }
+
+    public NodeInfo(String ipAddress, int portNumber)
+    {
+        this(ipAddress, portNumber, null);
+    }
+
+    String getAddress()
+    {
+        return this.ipAddress;
+    }
+
+    int getPort()
+    {
+        return this.portNumber;
+    }
+
+    String getName()
+    {
+        return this.name;
+    }
+}
