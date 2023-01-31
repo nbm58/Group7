e72cadc5628c659365f9d261401cdd87c943d260
Johnathan Ray <jdr479@nau.edu>
2023-01-30 19:32:13 -0700

    Fixed indentation in some files, added comments, & wrote additional error messages

--- ChatProject/src/utils/Receiver.java
+++ ChatProject/src/utils/Receiver.java
@@ -6,46 +6,49 @@ import java.net.ServerSocket;
 
 public class Receiver extends Thread
 {
- //client-side
- //receives messages from the chat server
- //init receiver ServerSocket
- static ServerSocket receiverSocket = null;
- //init client username
- static String userName = null;
+	 // Client-side
+	 // Receives messages from the chat server
+	 // Init receiver ServerSocket
+	 static ServerSocket receiverSocket = null;
+ 
+	 // Init client username
+	 static String userName = null;
 
-  //Constructor
- public Receiver()
- {
-  try
-  {
-   receiverSocket = new ServerSocket(ChatClient.myNodeInfo.getPort());
-   System.out.println("{RECEIVER} Socket created, listening on port " + ChatClient.myNodeInfo.getPort());
-  }
-  catch (IOException ex)
-  {
-   //Log failure to create socket
-   System.out.println("{RECEIVER} Failure to create socket");  
-  }
-  //display listening on message, port
-  System.out.println("{RECEIVER} " + ChatClient.myNodeInfo.getName() + " listening on " + ChatClient.myNodeInfo.getAddress() + ":" + ChatClient.myNodeInfo.getPort());
- }
+	 // Constructor
+	 public Receiver()
+	 {
+		 try
+		 {
+			 receiverSocket = new ServerSocket(ChatClient.myNodeInfo.getPort());
+			 System.out.println("{RECEIVER} Socket created, listening on port " + ChatClient.myNodeInfo.getPort());
+		 }
+		 catch (IOException ex)
+		 {
+			 // Log failure to create socket
+			 System.out.println("{RECEIVER} Failure to create socket");  
+		 }
 
- @Override
- public void run()
- {
-  //run server loop
-  while(true)
-  {
-   try
-   {
-	System.out.println("{RECEIVER} Waiting for message from server..");
-    (new ReceiverWorker(receiverSocket.accept())).start();
-   }
-   catch (IOException e)
-   {
-    //Log failure to create Socket Thread Object "error accepting client"
-   }
+		 // Display listening on message, port
+		 System.out.println("{RECEIVER} " + ChatClient.myNodeInfo.getName() + " listening on " + ChatClient.myNodeInfo.getAddress() + ":" + ChatClient.myNodeInfo.getPort());
+	 }
+
+	 @Override
+	 public void run()
+	 {
+		 // Run server loop
+		 while(true)
+		 {
+			 try
+			 {
+				 System.out.println("{RECEIVER} Waiting for message from server..");
+				 (new ReceiverWorker(receiverSocket.accept())).start();
+			 }
+			 catch (IOException e)
+			 {
+				 // Log failure to create Socket Thread Object "error accepting client"
+				 System.err.println("Error accepting client");
+			 }
    
-  }
- }    
+		 }
+	 }    
 }
--- ChatProject/src/utils/Sender.java
+++ ChatProject/src/utils/Sender.java
@@ -8,203 +8,225 @@ import java.util.Scanner;
 
 public class Sender extends Thread implements MessageTypes
 {
-
- //sender processes user input, translates to messages
- //and sends to chat server
- Socket serverConnection = null;
- Scanner userInput = new Scanner(System.in);
- String inputLine = null;
- boolean hasJoined;
+	// Sender processes user input, translates to messages
+	// and sends to chat server
+	Socket serverConnection = null;
+	Scanner userInput = new Scanner(System.in);
+	String inputLine = null;
+	boolean hasJoined;
 	
 	
- /*
-   Constructor
- */
- public Sender()
- {
-  userInput = new Scanner(System.in);
-  hasJoined = false;
- }
-	
- @Override
- public void run()
- {
-  System.out.println("{SENDER} Boot");
-  ObjectOutputStream writeToNet;
-  ObjectInputStream readFromNet;
-	 
-  //until SHUTDOWN/SHUTDOWN_ALL
-  while(true)
-  {
-   System.out.println("{SENDER} Enter Command:");
-   //get user input
-   inputLine = userInput.nextLine(); 
-	  
-   if (inputLine.startsWith("JOIN"))
-   {
-	System.out.println("{SENDER} command: JOIN");   
-	if(hasJoined == true)
+   // Constructor
+	public Sender()
 	{
-     //Log Chat already joined
-	 System.out.println("{SENDER} You have already joined a chat");
-	 continue;
+		userInput = new Scanner(System.in);
+		hasJoined = false;
 	}
 	
-   //read server information the user has provided, with JOIN command
-   String[] connectivityInfo = inputLine.split("[ ]+");
+	@Override
+	public void run()
+	{
+		System.out.println("{SENDER} Boot");
+		ObjectOutputStream writeToNet;
+		ObjectInputStream readFromNet;
+	 
+		// Loop until user runs SHUTDOWN or SHUTDOWN_ALL
+		while(true)
+		{
+			System.out.println("{SENDER} Enter Command:");
+			
+			// Get user input
+			inputLine = userInput.nextLine(); 
+	  
+			// JOIN functionality
+			if (inputLine.startsWith("JOIN"))
+			{
+				System.out.println("{SENDER} command: JOIN");   
+				if (hasJoined == true)
+				{
+					// Log Chat already joined
+					System.out.println("{SENDER} You have already joined a chat");
+					continue;
+				}
 	
-   //If there is any info that may override the connectivity information,
-   //that was provided through the properties
-   try
-   {
-	ChatClient.serverNodeInfo = new NodeInfo(connectivityInfo[1],
+				// Read server information the user has provided, with JOIN command
+				String[] connectivityInfo = inputLine.split("[ ]+");
+	
+				// If there is any info that may override the connectivity information,
+				// that was provided through the properties
+				try
+				{
+					ChatClient.serverNodeInfo = new NodeInfo(connectivityInfo[1],
 			                            Integer.parseInt(connectivityInfo[2]));
-   }
-   catch(ArrayIndexOutOfBoundsException ex)
-   {
-	// Don't do anything
-   }
+				}
+				catch(ArrayIndexOutOfBoundsException ex)
+				{
+					// Don't do anything
+				}
    
-   
-   try
-   {
-	//open connection to server
-	serverConnection = new Socket(ChatClient.serverNodeInfo.getAddress(), 
+				try
+				{
+					// Open connection to server
+					serverConnection = new Socket(ChatClient.serverNodeInfo.getAddress(), 
 			                              ChatClient.serverNodeInfo.getPort());
 	
-	//open object streams
-	readFromNet = new ObjectInputStream(serverConnection.getInputStream());
-	writeToNet = new ObjectOutputStream(serverConnection.getOutputStream());
+					// Open object streams
+					readFromNet = new ObjectInputStream(serverConnection.getInputStream());
+					writeToNet = new ObjectOutputStream(serverConnection.getOutputStream());
 	
-	//send join request
-	writeToNet.writeObject(new Message(JOIN, ChatClient.myNodeInfo));
-   }
-   catch(IOException ex)
-   {
-	//Log failure to connect
-	System.out.println("{SENDER} Failure to write JOIN");
-	continue;
-   }
-   hasJoined = true;   
-   System.out.println("{SENDER} Joined chat...");
-  }
-  else if (inputLine.startsWith("LEAVE"))
-  {
-   System.out.println("{SENDER} command: LEAVE");	  
-   if (hasJoined == false)
-   {
-    //Log chat not joined yet
-    continue;
-   }
-   try
-   {
-	//open connection to server
-	serverConnection = new Socket(ChatClient.serverNodeInfo.getAddress(), 
+					// Send join request
+					writeToNet.writeObject(new Message(JOIN, ChatClient.myNodeInfo));
+				}
+				catch (IOException ex)
+				{
+					// Log failure to connect
+					System.err.println("{SENDER} Failure to write JOIN");
+					continue;
+				}
+				hasJoined = true;   
+				System.out.println("{SENDER} Joined chat...");
+			}
+			
+			// LEAVE functionality
+			else if (inputLine.startsWith("LEAVE"))
+			{
+				System.out.println("{SENDER} command: LEAVE");
+				if (hasJoined == false)
+				{
+					// Log chat not joined yet
+					System.out.println("You have not joined the chat yet.");
+					continue;
+				}
+				try
+				{
+					// Open connection to server
+					serverConnection = new Socket(ChatClient.serverNodeInfo.getAddress(), 
 			                              ChatClient.serverNodeInfo.getPort());
 	
-	//open object streams
-	readFromNet = new ObjectInputStream(serverConnection.getInputStream());
-	writeToNet = new ObjectOutputStream(serverConnection.getOutputStream());
+					// Open object streams
+					readFromNet = new ObjectInputStream(serverConnection.getInputStream());
+					writeToNet = new ObjectOutputStream(serverConnection.getOutputStream());
 	
-	//send leave request
-	writeToNet.writeObject(new Message(LEAVE, ChatClient.myNodeInfo));
+					// Send leave request
+					writeToNet.writeObject(new Message(LEAVE, ChatClient.myNodeInfo));
 	
-	//close connection
-	serverConnection.close();
-   }
-   catch(IOException ex)
-   {
-	//Log failure to connect to server, or open/write/read obest stream/ or closing connection
-	continue;
-   }  
-   hasJoined = false;
-   System.out.println("Left chat...");
-  }
-  else if (inputLine.startsWith("SHUTDOWN_ALL"))
-  {
-	  System.out.println("{SENDER} command: SHUTDOWN_ALL");	  
-   if (hasJoined == false)
-   {
-    //Log join a chat before shutting it down
-    continue;
-   }
-   try
-    {
-	 //open connection to server
-	 serverConnection = new Socket(ChatClient.serverNodeInfo.getAddress(), ChatClient.serverNodeInfo.getPort());
-	 //open object streams
-	 readFromNet = new ObjectInputStream(serverConnection.getInputStream());
-	 writeToNet = new ObjectOutputStream(serverConnection.getOutputStream());
-	 //send shutdown_all request
-	 writeToNet.writeObject(new Message(SHUTDOWN_ALL, ChatClient.myNodeInfo));
-	 //close connection
-	 serverConnection.close();
-    }
-   catch(IOException ex)
-   {
-	//Log shutdown failure
-	continue;
-   }  
-   System.out.println("Sent shutdown all request...");
-  }
-  else if (inputLine.startsWith("SHUTDOWN"))
-  {
-	  System.out.println("{SENDER} command: SHUTDOWN");
-   //Leave if participant, before shutdown
-   if (hasJoined == true)
-   {
-    //Send leave request
-    try
-    {
-	 //open connection to server
-	 serverConnection = new Socket(ChatClient.serverNodeInfo.getAddress(), ChatClient.serverNodeInfo.getPort());
-	 //open object streams
-	 readFromNet = new ObjectInputStream(serverConnection.getInputStream());
-	 writeToNet = new ObjectOutputStream(serverConnection.getOutputStream());	
- 	 //send shutdown_all request
-  	 writeToNet.writeObject(new Message(SHUTDOWN, ChatClient.myNodeInfo));
-	 //close connection
-	 serverConnection.close();
-	 System.out.println("Left chat...");
-    }
-    catch(IOException ex)
-    {
-	 //Log shutdown failure
-	 continue;
-    }  
-    System.out.println("Exiting...");
-    System.exit(0);
-   }   
-  }
-  else
-  {
-   if(hasJoined == false)
-   {
-    //Log Join chat first!
-	System.out.println("You must join a chat before you can post notes!");   
-	continue;
-   }
-   try
-   {
-	   System.out.println("{SENDER} command: NOTE");
-	//open connection to server
-	serverConnection = new Socket(ChatClient.serverNodeInfo.getAddress(), ChatClient.serverNodeInfo.getPort());	
-	//open object streams
-	writeToNet = new ObjectOutputStream(serverConnection.getOutputStream());	
-	//send shutdown_all request
-	writeToNet.writeObject(new Message(NOTE, ChatClient.myNodeInfo));
+					// Close connection
+					serverConnection.close();
+				}
+				catch(IOException ex)
+				{
+					// Log failure to connect to server, or open/write/read stream, or closing connection
+					System.err.println("{SENDER} Failure to write LEAVE");
+					continue;
+				}  
+				hasJoined = false;
+				System.out.println("Left chat...");
+			}
+			
+			// SHUTDOWN_ALL functionality
+			else if (inputLine.startsWith("SHUTDOWN_ALL"))
+			{
+				System.out.println("{SENDER} command: SHUTDOWN_ALL");	  
+				if (hasJoined == false)
+				{
+					// Log join a chat before shutting it down
+					System.out.println("You have not joined the chat yet.");
+					continue;
+				}
+				try
+				{
+					// Open connection to server
+					serverConnection = new Socket(ChatClient.serverNodeInfo.getAddress(), ChatClient.serverNodeInfo.getPort());
+					
+					// Open object streams
+					readFromNet = new ObjectInputStream(serverConnection.getInputStream());
+					writeToNet = new ObjectOutputStream(serverConnection.getOutputStream());
+					
+					// Send shutdown_all request
+					writeToNet.writeObject(new Message(SHUTDOWN_ALL, ChatClient.myNodeInfo));
+	 
+					// Close connection
+					serverConnection.close();
+				}
+				catch(IOException ex)
+				{
+					// Log shutdown failure
+					System.err.println("{SENDER} Failure to write SHUTDOWN_ALL");
+					continue;
+				}  
+				System.out.println("Sent shutdown all request...");
+			}
+			
+			// SHUTDOWN functionality
+			else if (inputLine.startsWith("SHUTDOWN"))
+			{
+				System.out.println("{SENDER} command: SHUTDOWN");
+				// Leave if participant, before shutdown
+				
+				if (hasJoined == true)
+				{
+					// Send leave request
+					try
+					{
+						// Open connection to server
+						serverConnection = new Socket(ChatClient.serverNodeInfo.getAddress(), ChatClient.serverNodeInfo.getPort());
+						
+						// Open object streams
+						readFromNet = new ObjectInputStream(serverConnection.getInputStream());
+						writeToNet = new ObjectOutputStream(serverConnection.getOutputStream());	
+						
+						// Send shutdown_all request
+						writeToNet.writeObject(new Message(SHUTDOWN, ChatClient.myNodeInfo));
+	 
+						// Close connection
+						serverConnection.close();
+						System.out.println("Left chat...");
+					}
+					catch(IOException ex)
+					{
+						// Log shutdown failure
+						System.err.println("{SENDER} Failure to write SHUTDOWN");
+						continue;
+					}  
+					System.out.println("Exiting...");
+					System.exit(0);
+				}   
+			}
+			
+			// NOTE functionality
+			else
+			{
+				if(hasJoined == false)
+				{
+					//Log Join chat first!
+					System.out.println("You must join a chat before you can post notes!");   
+					continue;
+				}
+				try
+				{
+					System.out.println("{SENDER} command: NOTE");
 	
+					// Open connection to server
+					serverConnection = new Socket(ChatClient.serverNodeInfo.getAddress(), ChatClient.serverNodeInfo.getPort());	
+					
+					// Open object streams
+					writeToNet = new ObjectOutputStream(serverConnection.getOutputStream());	
 	
-	//close connection
-	serverConnection.close();
-	System.out.println("Message Sent...");
-   }
-   catch(IOException ex)
-   {
-	//Log Note send failure
-    continue;
-   } 
-  }
- }
-}   
+					// Send shutdown_all request
+					writeToNet.writeObject(new Message(NOTE, ChatClient.myNodeInfo));
+	
+					// Close connection
+					serverConnection.close();
+	
+					System.out.println("Message Sent...");
+				}
+				catch(IOException ex)
+				{
+					// Log Note send failure
+					System.err.println("{SENDER} Failure to write NOTE");
+					continue;
+				} 
+			}
+		}
+	}   
 }
