17a5df8918a4d191663f337efc25f3be7095ece2
tam499 <tam499@nau.edu>
2023-04-02 20:03:58 -0700

    add client/server properties files and grab properties for client in constructor

--- Project3-TransactionServer/src/main/java/client/TransactionClient.java
+++ Project3-TransactionServer/src/main/java/client/TransactionClient.java
@@ -4,22 +4,62 @@ import utils.*;
 import java.util.*;
 import java.io.*;
 
+import message.*;
+
 public class TransactionClient implements Runnable
 {
     PropertyHandler properties;
+    String serverIP;
+    int serverPort;
     int numberOfTransactions;
+    boolean restartTransactions;
+    int numberOfAccounts;
     int startingBalance;
+    int accountFrom;
+    int accountTo;
     
-    public TransactionClient(String propertiesFile)
+    public TransactionClient(String clientPropertiesFile, String serverPropertiesFile)
     {
-        Properties properties = null;
+        Properties clientProperties = null;
+        Properties serverProperties = null;
+        
         try
         {
-            properties = new PropertyHandler(propertiesFile);
+            clientProperties = new PropertyHandler(clientPropertiesFile);
         }
         catch (IOException ex)
         {
-            System.err.println("Could not open properties file" + ex);
+            System.err.println("Could not open client properties file" + ex);
+            System.exit(1);
+        }
+        
+        try
+        {
+            serverProperties = new PropertyHandler(serverPropertiesFile);
+        }
+        catch (IOException ex)
+        {
+            System.err.println("Could not open server properties file" + ex);
+            System.exit(1);
+        }
+        
+        // get server ip
+        serverIP = serverProperties.getProperty("SERVER_IP");
+        if (serverIP == null)
+        {
+            System.err.println("Error getting server IP: ");
+            System.exit(1);
+        }
+        
+        // get server port
+        serverPort = 0;
+        try
+        {
+            serverPort = Integer.parseInt(serverProperties.getProperty("SERVER_PORT"));
+        }
+        catch (NumberFormatException ex)
+        {
+            System.err.println("Error getting server port: " + ex);
             System.exit(1);
         }
         
@@ -27,7 +67,7 @@ public class TransactionClient implements Runnable
         numberOfTransactions = 0;
         try
         {
-            numberOfTransactions = Integer.parseInt(properties.getProperty("NUMBER_OF_TRANSACTIONS"));
+            numberOfTransactions = Integer.parseInt(clientProperties.getProperty("NUMBER_OF_TRANSACTIONS"));
         }
         catch (NumberFormatException ex)
         {
@@ -35,17 +75,44 @@ public class TransactionClient implements Runnable
             System.exit(1);
         }
         
-        // get number of transactions
+        // get restart transactions flag
+        restartTransactions = true;
+        try
+        {
+            restartTransactions = Boolean.parseBoolean(clientProperties.getProperty("RESTART_TRANSACTIONS"));
+        }
+        catch (NumberFormatException ex)
+        {
+            System.err.println("Error getting restart transactions flag: " + ex);
+            System.exit(1);
+        }
+        
+        // get number of accounts
+        numberOfAccounts = 0;
+        try
+        {
+            numberOfAccounts = Integer.parseInt(serverProperties.getProperty("NUMBER_OF_ACCOUNTS"));
+        }
+        catch (NumberFormatException ex)
+        {
+            System.err.println("Error getting number of accounts: " + ex);
+            System.exit(1);
+        }
+        
+        // get starting balance
         startingBalance = 0;
         try
         {
-            startingBalance = Integer.parseInt(properties.getProperty("STARTING_BALANCE"));
+            startingBalance = Integer.parseInt(serverProperties.getProperty("STARTING_BALANCE"));
         }
         catch (NumberFormatException ex)
         {
             System.err.println("Error getting starting balance: " + ex);
             System.exit(1);
         }
+        
+        accountFrom = (int) (Math.floor(Math.random() * startingBalance));
+        accountTo = (int) (Math.floor(Math.random() * startingBalance));
     }
     
     void openTransaction(int transactionID)
@@ -56,6 +123,16 @@ public class TransactionClient implements Runnable
     void closeTransaction(int transactionID)
     {
         
+    }
+    
+    public class TransactionThread extends Thread implements MessageTypes, Runnable
+    {
+        
+        @Override
+        public void run()
+        {
+            (new TransactionServerProxy()).run();
+        }
     }
     
     @Override
